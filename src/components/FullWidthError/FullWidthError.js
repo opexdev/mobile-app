34adec0558a0b9bc53ffe3e64b4a8182aa4c3981
Hossein <hossein13785ab@gmail.com>
2022-10-17 12:13:31 +0330

    Fix fullWidthError component

--- src/components/FullWidthError/FullWidthError.js
+++ src/components/FullWidthError/FullWidthError.js
@@ -21,20 +21,18 @@ const FullWidthError = () => {
         <div className={`container ${classes.container} move-image flex jc-center ai-center text-red`} style={{backgroundImage: `url("${images.spaceStar}")`}}>
             <div className={`column jc-around ai-center card-border py-1`} style={{
                 position: 'fixed',
-                top: '27.5%',
-                left: '37.5%',
-                width: '25%',
-                height: '45%',
+                width: '70%',
+                height: '50%',
                 backgroundColor: 'var(--cardBodyAlpha)',
             }}>
-                <img src={images.reject} alt="error" className={`img-lg-plus floating`}/>
+                <img src={images.reject} alt="error" className={`img-lg-05 floating`}/>
                 <span>{t("errorPage.errorText")}</span>
                 <Button
                     buttonStyle={{
                         background: 'var(--bgGreen)',
                         color: '#000'
                     }}
-                    buttonClass={`px-2`}
+                    buttonClass={`px-5`}
                     buttonTitle={t("errorPage.reload")}
                     onClick={tryLoadConfig}
                 />
