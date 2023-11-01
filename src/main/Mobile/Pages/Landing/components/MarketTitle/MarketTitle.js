7f2f7f8a6969746e0d511e29c7b812e15d5211b3
Hossein <hossein13785ab@gmail.com>
2023-11-01 18:22:50 +0330

    #40: Add market-order comp

--- src/main/Mobile/Pages/Landing/components/MarketTitle/MarketTitle.js
+++ src/main/Mobile/Pages/Landing/components/MarketTitle/MarketTitle.js
@@ -4,7 +4,7 @@ import Title from "../../../../../../components/Title/Title";
 import {useTranslation} from "react-i18next";
 import Button from "../../../../../../components/Button/Button";
 import {useNavigate} from "react-router-dom";
-import {Overview} from "../../../../Routes/routes";
+import {EasyTrading, Overview} from "../../../../Routes/routes";
 
 const MarketTitle = () => {
     const {t} = useTranslation();
@@ -13,16 +13,16 @@ const MarketTitle = () => {
     return (
         <div className={`width-90 column m-auto py-3`}>
             <Title title={t('title')}/>
-            <div className={`row jc-center ai-center mt-2`}>
-                {/*<Button
-                    buttonClass={`${classes.thisButton} col-48 px-3`}
+            <div className={`row jc-between ai-center mt-2`}>
+                <Button
+                    buttonClass={`${classes.thisButton} width-48`}
                     buttonTitle={t("MarketTitle.easyTrading")}
                     type="submit"
-                    onClick={() => navigate("/", { replace: true })}
+                    onClick={() => navigate(EasyTrading)}
 
-                />*/}
+                />
                 <Button
-                    buttonClass={`${classes.thisButton} col-48 px-3`}
+                    buttonClass={`${classes.thisButton} width-48`}
                     buttonTitle={t("MarketTitle.advancedTrading")}
                     type="submit"
                     onClick={() => navigate(Overview)}
