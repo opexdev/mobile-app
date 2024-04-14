4264926422b79c546684e9968af94d7c83d5acb9
Hossein <hossein13785ab@gmail.com>
2024-04-14 17:14:48 +0330

    Fix expand option in subHeader

--- src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/MarketSubHeader/MarketSubHeader.js
+++ src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/MarketSubHeader/MarketSubHeader.js
@@ -21,22 +21,22 @@ const MarketSubHeader = () => {
         <>
             <div className={`width-100 row jc-between ai-center my-05`}>
                <span >{t("header.lastPrice")}</span>
-               <span>{new BN(lastPrices[activePair.symbol] || 0).toFormat()+"  "+t("currency." + activePair.quoteAsset)}</span>
             </div>
-
-            <div className={`width-100 row jc-between ai-center my-05`}>
-                <span>{t("header.availableBalance")}</span>
-                <div className={`row ai-center ${classes.inventory} mt-05`}>
-                    <div className=" flex ai-center jc-end">
-                        {/*<span>{ new BN (base).decimalPlaces(activePair.baseAssetPrecision).toFormat()}</span>*/}
-                        <span className={`pl-05`}>{ new BN (base).toFormat()}</span>
-                        <span className={`pr-05`}>{t("currency." + activePair.baseAsset)}</span>
-                    </div>
-                    <span className={`text-orange mx-2`}>|</span>
-                    <div className=" flex ai-center  jc-start">
-                        <span className={`pl-05`}>{ new BN(quote).toFormat()}</span>
-                        <span className={`pr-05`}>{t("currency." + activePair.quoteAsset)}</span>
-                    </div>
+            <div className={`width-100 row jc-between ai-start mb-1`}>
+                <span>{new BN(lastPrices[activePair.symbol] || 0).toFormat()+"  "+t("currency." + activePair.quoteAsset)}</span>
+            </div>
+            <div className={`width-100 row jc-between ai-start my-05`}>
+                <div>{t("header.availableBalance")}</div>
+            </div>
+            <div className={`width-100 row jc-start ai-start mb-1`}>
+                <div className=" row ai-center jc-end">
+                    <span className={`pl-05`}>{ new BN (base).toFormat()}</span>
+                    <span className={`pr-05`}>{t("currency." + activePair.baseAsset)}</span>
+                </div>
+                <span className={`text-orange mx-3`}>|</span>
+                <div className=" row ai-center  jc-end">
+                    <span className={`pl-05`}>{ new BN(quote).toFormat()}</span>
+                    <span className={`pr-05`}>{t("currency." + activePair.quoteAsset)}</span>
                 </div>
             </div>
         </>
