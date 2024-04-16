9ce5eccf134fc1f17370c196990cf67d68c40b13
Hossein <hossein13785ab@gmail.com>
2024-04-16 14:32:51 +0330

    Close #58: Handle Georgian & Solar Hijri calendar type in transactions history filter

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -3,8 +3,12 @@ import Icon from "../Icon/Icon";
 import Select from "react-select";
 import classes from "./TextInput.module.css";
 import DatePicker from "react-multi-date-picker";
-import persian_fa from "react-date-object/locales/persian_fa";
+import gregorian from "react-date-object/calendars/gregorian";
 import persian from "react-date-object/calendars/persian";
+import englishGregorian from "react-date-object/locales/gregorian_en";
+import farsiGregorian from "react-date-object/locales/gregorian_fa";
+import englishJalali from "react-date-object/locales/persian_en";
+import farsiJalali from "react-date-object/locales/persian_fa";
 import {useSelector} from "react-redux";
 import i18n from "i18next";
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
@@ -25,6 +29,21 @@ const TextInput = (props) => {
         }
         return className;
     }
+
+    const calenderTypeHandler = () => {
+        if (type === "Hijri" && i18n.language === "en") {
+            return englishGregorian
+        }
+        if (type === "Hijri" && i18n.language === "fa") {
+            return farsiGregorian
+        }
+        if (type === "Jalali" && i18n.language === "en") {
+            return englishJalali
+        }
+        if (type === "Jalali" && i18n.language === "fa") {
+            return farsiJalali
+        }
+    };
 
 
     let leadSection = null
@@ -64,8 +83,8 @@ const TextInput = (props) => {
     if ( datePicker ){
         inputSection = <DatePicker
             className={`${theme === "DARK" && "bg-dark"}`}
-            locale={i18n.language === "fa" ? persian_fa : null}
-            calendar={type === "Jalali" ? persian : null}
+            locale={calenderTypeHandler()}
+            calendar={type === "Jalali" ? persian : gregorian}
             onChange={onchange}
             render={<input className={`${classes.datePicker}`}/>}
             {...other}
