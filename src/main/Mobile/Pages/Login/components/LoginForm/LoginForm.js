e47e39f87260f259db0a75eea7eb01cdfa67787a
Hossein <hossein13785ab@gmail.com>
2025-01-06 14:02:34 +0330

    fix: ensure setShowVerifyEmail is false for all error cases

--- src/main/Mobile/Pages/Login/components/LoginForm/LoginForm.js
+++ src/main/Mobile/Pages/Login/components/LoginForm/LoginForm.js
@@ -92,9 +92,11 @@ const LoginForm = () => {
             })
             .catch((err) => {
                 if (err?.response?.status === 401) {
+                    setShowVerifyEmail(false)
                     return setLoginError(t("login.wrongPassword"));
                 }
                 if (err?.response?.status === 403) {
+                    setShowVerifyEmail(false)
                     setLoginError(t("login.wrongOTP"));
                     return setNeedOTP(true)
                 }
@@ -102,6 +104,7 @@ const LoginForm = () => {
                     setShowVerifyEmail(true)
                     return setLoginError(t("login.accountNotActive"));
                 }
+                setShowVerifyEmail(false)
                 setLoginError(t("login.loginError"));
             })
             .finally(() => {
