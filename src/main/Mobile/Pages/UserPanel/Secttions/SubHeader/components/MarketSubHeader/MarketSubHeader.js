f51b9523263e0a47316e3a9954504ca74d79a054
Hossein <hossein13785ab@gmail.com>
2025-04-21 19:12:53 +0330

    #85 feat: Integrated sub-header of Wallet and Market with Currency Service

--- src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/MarketSubHeader/MarketSubHeader.js
+++ src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/MarketSubHeader/MarketSubHeader.js
@@ -3,14 +3,19 @@ import {useSelector} from "react-redux";
 import {useTranslation} from "react-i18next";
 import {useGetLastPrices} from "../../../../../../../../queries/hooks/useGetLastPrices";
 import {useGetUserAccount} from "../../../../../../../../queries/hooks/useGetUserAccount";
-import {BN} from "../../../../../../../../utils/utils";
+import {BN, getCurrencyNameOrAlias} from "../../../../../../../../utils/utils";
 import classes from "./MarketSubHeader.module.css"
+import i18n from "i18next";
 
 const MarketSubHeader = () => {
 
     const {t} = useTranslation();
 
+    const language = i18n.language
+    const currencies = useSelector((state) => state.exchange.currencies)
+
     const activePair = useSelector((state) => state.exchange.activePair)
+
     const {data: lastPrices} = useGetLastPrices()
     const {data: userAccount} = useGetUserAccount()
 
@@ -23,20 +28,20 @@ const MarketSubHeader = () => {
                <span >{t("header.lastPrice")}</span>
             </div>
             <div className={`width-100 row jc-between ai-start mb-1`}>
-                <span>{new BN(lastPrices[activePair.symbol] || 0).toFormat()+"  "+t("currency." + activePair.quoteAsset)}</span>
+                <span>{" "}{new BN(lastPrices[activePair.symbol] || 0).decimalPlaces(currencies[activePair?.quoteAsset]?.precision ?? 0).toFormat() +" "+ getCurrencyNameOrAlias(currencies[activePair?.quoteAsset], language)}</span>
             </div>
             <div className={`width-100 row jc-between ai-start my-05`}>
                 <div>{t("header.availableBalance")}</div>
             </div>
             <div className={`width-100 row jc-start ai-start mb-1`}>
                 <div className=" row ai-center jc-end">
-                    <span className={`pl-05`}>{ new BN (base).toFormat()}</span>
-                    <span className={`pr-05`}>{t("currency." + activePair.baseAsset)}</span>
+                    <span className={`pl-05`}>{new BN (base).decimalPlaces(currencies[activePair?.baseAsset]?.precision ?? 0).toFormat()}</span>
+                    <span className={`pr-05`}>{getCurrencyNameOrAlias(currencies[activePair?.baseAsset], language)}</span>
                 </div>
                 <span className={`text-orange mx-3`}>|</span>
                 <div className=" row ai-center  jc-end">
-                    <span className={`pl-05`}>{ new BN(quote).toFormat()}</span>
-                    <span className={`pr-05`}>{t("currency." + activePair.quoteAsset)}</span>
+                    <span className={`pl-05`}>{new BN(quote).decimalPlaces(currencies[activePair?.quoteAsset]?.precision ?? 0).toFormat()}</span>
+                    <span className={`pr-05`}>{getCurrencyNameOrAlias(currencies[activePair?.quoteAsset], language)}</span>
                 </div>
             </div>
         </>
--- src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/WalletSubHeader/WalletSubHeader.js
+++ src/main/Mobile/Pages/UserPanel/Secttions/SubHeader/components/WalletSubHeader/WalletSubHeader.js
@@ -4,13 +4,19 @@ import {useParams} from "react-router-dom";
 import {useGetUserAssets} from "../../../../../../../../queries";
 import {useTranslation} from "react-i18next";
 import {useGetUserAccount} from "../../../../../../../../queries/hooks/useGetUserAccount";
-import {BN} from "../../../../../../../../utils/utils";
+import {BN, formatWithPrecision} from "../../../../../../../../utils/utils";
+import i18n from "i18next";
+import {useSelector} from "react-redux";
 
 const WalletSubHeader = () => {
 
     const {id} = useParams()
     const {t} = useTranslation()
     const refCurrency = window.env.REACT_APP_REFERENCE_FIAT_CURRENCY
+
+    const language = i18n.language
+    const currencies = useSelector((state) => state.exchange.currencies)
+
     const {data: userAccount} = useGetUserAccount()
 
     const {data: estimateValue , isLoading, error} = useGetUserAssets(refCurrency)
@@ -21,18 +27,18 @@ const WalletSubHeader = () => {
 
             <div className={`col-35 column ai-center`}>
                 <span className={`mb-05 text-center`}>{t("header.free")}</span>
-                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.free || 0).toFormat()}</span>
-                <span className={`fs-0-7 text-gray`}> ( {refCurrency === id ? new BN(userAccount?.wallets[id]?.free || 0).toFormat() : new BN(allEstimateValue?.free || 0).toFormat()} {t("currency."+refCurrency)} )</span>
+                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.free || 0).decimalPlaces(currencies[id]?.precision ?? 0).toFormat()}</span>
+                <span className={`fs-0-7 text-gray`}> ({ refCurrency === id ? formatWithPrecision(userAccount?.wallets[id]?.free || 0, currencies[id]?.precision ?? 0) : formatWithPrecision(allEstimateValue?.free || 0, currencies[refCurrency]?.precision ?? 0)} {refCurrency} )</span>
             </div>
             <div className={`col-35 column ai-center ${classes.border}`}>
                 <span className={`mb-05 text-center`}>{t("header.locked")}</span>
-                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.locked || 0).toFormat()}</span>
-                <span className={`fs-0-7 text-gray`}> ( {refCurrency === id ? new BN(userAccount?.wallets[id]?.locked || 0).toFormat() : new BN(allEstimateValue?.locked || 0).toFormat()} {t("currency."+refCurrency)} )</span>
+                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.locked || 0).decimalPlaces(currencies[id]?.precision ?? 0).toFormat()}</span>
+                <span className={`fs-0-7 text-gray`}> ({ refCurrency === id ? formatWithPrecision(userAccount?.wallets[id]?.locked || 0, currencies[id]?.precision ?? 0) : formatWithPrecision(allEstimateValue?.locked || 0, currencies[refCurrency]?.precision ?? 0)} {refCurrency} )</span>
             </div>
             <div className={`col-35 column ai-center`}>
                 <span className={`mb-05 text-center`}>{t("header.inWithdrawalProcess")}</span>
-                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.withdraw || 0).toFormat()}</span>
-                <span className={`fs-0-7 text-gray`}> ( {refCurrency === id ? new BN(userAccount?.wallets[id]?.withdraw || 0).toFormat() : new BN(allEstimateValue?.withdrawing || 0).toFormat()} {t("currency."+refCurrency)} )</span>
+                <span className={`mt-05`}>{new BN(userAccount?.wallets[id]?.withdraw || 0).decimalPlaces(currencies[id]?.precision ?? 0).toFormat()}</span>
+                <span className={`fs-0-7 text-gray`}> ({ refCurrency === id ? formatWithPrecision(userAccount?.wallets[id]?.withdraw || 0, currencies[id]?.precision ?? 0) : formatWithPrecision(allEstimateValue?.withdrawing || 0, currencies[refCurrency]?.precision ?? 0)} {refCurrency} )</span>
             </div>
         </div>
     );
